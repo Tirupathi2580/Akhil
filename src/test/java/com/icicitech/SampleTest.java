9af0ff17bebe5dd1730e9136a6f477cbcf10bb61
Tirupathi2580 <tirupathirao2580@gmail.com>
2023-11-22 07:11:37 +0530

    third message

---
+++ src/test/java/com/icicitech/SampleTest.java
@@ -0,0 +1,24 @@
+package com.icicitech;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import my.package.com.sampletestapp;
+
+/**
+ * Unit test for simpleApp.
+ */
+public class sampleapptest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+	System.out.println("testing passed");
+	
+    }
+}
